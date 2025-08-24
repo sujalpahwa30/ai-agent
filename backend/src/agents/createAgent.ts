6c9aba6591be1f218b62febfd0c3c2607e9c8576
Sujal Pahwa <sujalpahwa4@gmail.com>
2025-08-24 23:54:21 +0530

    feat: added create agent func and its types completely

--- backend/src/agents/createAgent.ts
+++ backend/src/agents/createAgent.ts
@@ -1,2 +1,28 @@
 import { StreamChat } from "stream-chat";
 import { apiKey, serverClient } from "../serverClient";
+import { AgentPlatform, AIAgent } from "./types";
+
+export const createAgent = async (
+    user_id: string,
+    platform: AgentPlatform,
+    channel_type: string,
+    channel_id: string 
+) : Promise<AIAgent> => {
+    const token = serverClient.createToken(user_id);
+    // This is the client for the AI bot user 
+    const chatClient = new StreamChat(apiKey, undefined, {
+        allowServerSideConnect: true,
+    });
+
+    await chatClient.connectUser({ id: user_id }, token);
+    const channel = chatClient.channel(channel_type, channel_id);
+    await channel.watch();
+
+    switch(platform) {
+        case AgentPlatform.WRITING_ASSISTANT:
+        case AgentPlatform.OPENAI:
+
+        default:
+            throw new Error(`Unsupported agent platform: ${platform}`);
+    }
+};
