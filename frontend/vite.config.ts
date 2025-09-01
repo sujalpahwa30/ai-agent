ca70d37db7bc8bd5667d1cea709aaf8983e3e0be
Sujal Pahwa <sujalpahwa4@gmail.com>
2025-09-01 17:48:21 +0530

    feat: added the input and label ui component

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
+import react from "@vitejs/plugin-react";
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
+// eslint-disable-next-line no-empty-pattern
+export default defineConfig(({ }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
