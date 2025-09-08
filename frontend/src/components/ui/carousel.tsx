a9f469d93ee803c50bd2986be93b25b384864605
Sujal Pahwa <sujalpahwa4@gmail.com>
2025-09-08 23:58:31 +0530

    added collapsible and started the carousel component

---
+++ frontend/src/components/ui/carousel.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import useEmblaCarousel, {
+    type UseEmblaCarouselType, 
+} from "embla-carousel-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
+
+import { cn } from "../../lib/utils";
+import { Button } from "./button";
+
+type CarouselApi = UseEmblaCarouselType[1];
+type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
+type CarouselOptions = UseCarouselParameters[0];
+type CarouselPlugin = UseCarouselParameters[1];
+
+type CarouselProps = {
+    opts?: CarouselOptions
+    plugins?: CarouselPlugin
+    orientation?: "horizontal" | "vertical"
+    setApi?: (api: CarouselApi) => void 
+}
+
+type CarouselContextProps = {
+    carouselRef: ReturnType<typeof useEmblaCarousel>[0]
+    api: ReturnType<typeof useEmblaCarousel>[1]
+    scrollPrev: () => void 
+    scrollNext: () => void 
+    canScrollPrev: boolean 
+    canScrollNext: boolean 
+} & CarouselProps 
+
+const CarouselContext = React.createContext<CarouselContextProps | null>(null); 
---
+++ frontend/src/components/ui/collapsible.tsx
@@ -0,0 +1,10 @@
+import * as CollapsiblePrimitve from "@radix-ui/react-collapsible";
+
+const Collapsible = CollapsiblePrimitve.Root;
+
+const CollapsibleTrigger = CollapsiblePrimitve.Trigger;
+
+const CollapsibleContent = CollapsiblePrimitve.CollapsibleContent;
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent }; 
+
